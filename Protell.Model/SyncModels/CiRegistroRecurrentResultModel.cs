a3170e5203d218af65d8e921a59ef6daae882ae3
slytsal <jsalazar@inmeta.com.mx>
2014-04-05 15:07:05 -0600

    Download de CI_REGISTRO Recurrente 2do ajuste

---
+++ Protell.Model/SyncModels/CiRegistroRecurrentResultModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+namespace Protell.Model.SyncModels
+{
+    [DataContract]
+    public class CiRegistroRecurrentResultModel : ModelBase
+    {
+        [DataMember]
+        public ObservableCollection<RegistroModel> Download_CIRegistroRecurrentResult
+        {
+            get { return _Download_CIRegistroRecurrentResult; }
+            set
+            {
+                if (_Download_CIRegistroRecurrentResult != value)
+                {
+                    _Download_CIRegistroRecurrentResult = value;
+                    OnPropertyChanged(Download_CIRegistroRecurrentPropertyName);
+                }
+            }
+        }
+        private ObservableCollection<RegistroModel> _Download_CIRegistroRecurrentResult;
+        public const string Download_CIRegistroRecurrentPropertyName = "Download_CIRegistroRecurrentResult";
+    }
+}
