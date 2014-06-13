d8a6a9f02d2822826d0ff22632d583e5f3682bc4
slytsal <jsalazar@inmeta.com.mx>
2014-06-12 22:09:44 -0500

    log sq

---
+++ Protell.DAL/Repository/v2/SQLLogger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Protell.DAL.Repository.v2
+{
+    public sealed class SQLLogger
+    {
+        private static readonly Lazy<SQLLogger> lazy = new Lazy<SQLLogger>(() => new SQLLogger());
+        public static SQLLogger Instance { get { return lazy.Value; } }
+        //Constructor
+        private SQLLogger()
+        {
+            string tmp = ConfigurationManager.AppSettings["SqlLogEnabled"].ToString();
+            this.enabled = false;
+            if (tmp == "1" || tmp == "true")
+            {
+                this.enabled = true;
+            }
+        }
+
+        private bool enabled;
+
+        public void log(Exception ex,string where)
+        {
+            this.log(this.ExceptionMessages(ex), where);
+        }
+
+        public void log(string msg, string where)
+        {
+            using (var entity = new db_SeguimientoProtocolo_r2Entities())
+            {
+                string sqlExists = " if object_id('templog','u') is null begin create table templog (id int identity(1,1) , msg nvarchar(max), t datetime) end";
+                entity.ExecuteStoreCommand(sqlExists);
+
+                string sqlInsert = "insert into templog(msg) select '" + where + " _ [" + msg + "]',getdate()";
+                entity.ExecuteStoreCommand(sqlInsert);
+            }
+        }
+
+        private string ExceptionMessages(Exception ex)
+        {
+            string msgs="";
+
+            if(ex!=null)
+            {
+                msgs += this.SafeSqlString(ex.Message);
+                if(ex.InnerException!=null && !String.IsNullOrEmpty( ex.InnerException.Message) )
+                {
+                    msgs += this.SafeSqlString(ex.InnerException.Message);
+                }
+            }
+
+            return msgs;
+        }
+
+        private string SafeSqlString(string str)
+        {
+            return str.Replace('\'', 'D');
+        }
+    }
+}
